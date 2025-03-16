65ebbb503022e2c854b9b78ae6032ee8c0bec840
Alexyycb <Amanda.da.silva2815@gmail.com>
2025-03-16 11:33:25 -0300

    Ajustes finais

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 function adicionarAmigo() {
     let amigo = document.querySelector('input').value;
-    console.log('Botão esta funcioando');
     if (amigo != "") {
         amigos.push(amigo);
-        console.log(amigos);
         limparCampo();
         atualizarLista(amigos);
-}
+    }
     else {
         alert('Por favor, insira um nome.');
     }
@@ -17,10 +14,10 @@ function limparCampo() {
     amigo = document.querySelector('input');
     amigo.value = "";
 }
-function atualizarLista (amigos) {
+function atualizarLista(amigos) {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = "";
-    for(let i = 0 ; i < amigos.length ; i++) {
+    for (let i = 0; i < amigos.length; i++) {
         let elemento = document.createElement('li');
         elemento.textContent = amigos[i];
         lista.appendChild(elemento);
@@ -28,15 +25,12 @@ function atualizarLista (amigos) {
 }
 function sortearAmigo() {
     if (amigos.length === 0) {
-    console.log('sortear vazio');
-    alert('Lista de amigos vazia, por favor insira um nome')
-    return;
-} else {
-    console.log('sortear não vazio');
-    let indiceSorteado = Math.floor(Math.random() * amigos.length);
-    let nomeSorteado = amigos[indiceSorteado];
-    console.log(nomeSorteado);
-    let resultadoFinal = document.getElementById('resultado');
-    resultadoFinal.innerHTML = nomeSorteado;
-}
+        alert('Lista de amigos vazia, por favor insira um nome')
+        return;
+    } else {
+        let indiceSorteado = Math.floor(Math.random() * amigos.length);
+        let nomeSorteado = amigos[indiceSorteado];
+        let resultadoFinal = document.getElementById('resultado');
+        resultadoFinal.innerHTML = nomeSorteado;
+    }
 }
