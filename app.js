5d6d829987883f487b5a6d9ab466ded8c2b99db8
Alexyycb <Amanda.da.silva2815@gmail.com>
2025-03-16 09:25:58 -0300

    Adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    console.log('Botão esta funcioando');
+    if (amigo != "") {
+        amigos.push(amigo);
+        console.log(amigos);
+        limparCampo();
+    }
+    else {
+        alert('Por favor, insira um nome.');
+    }
+}
+function limparCampo() {
+    amigo = document.querySelector('input');
+    amigo.value = "";
+}
